c7d9a5d87be3f5af5596edbb9dd247f762940a39
anDrekon <4594400@gmail.com>
2020-10-28 09:51:40 +0200

    Initial commit

---
+++ src/main/java/com/doc/rest/apprestws/AppRestWsApplication.java
@@ -0,0 +1,13 @@
+package com.doc.rest.apprestws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRestWsApplication.class, args);
+    }
+
+}
