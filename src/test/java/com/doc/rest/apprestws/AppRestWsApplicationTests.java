ee1b4ed8886974cbb9cd1a614326b9740fb04af2
anDrekon <4594400@gmail.com>
2020-10-28 13:07:02 +0200

    4. Create RESTful Web Service Endpoints

---
+++ src/test/java/com/doc/rest/apprestws/AppRestWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doc.rest.apprestws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
